d817750efa5946aed77690e52605d9d1fd15d869
Aron Roberts <robotros1@msn.com>
2017-03-04 19:40:34 -0800

    Major refactor of code
    seperated entity class into individual files.
    other refactoring

--- full-stack-nanodegree/Project-Multi-User-Blog/hello-udacity/main.py
+++ full-stack-nanodegree/Project-Multi-User-Blog/hello-udacity/main.py
@@ -23,124 +23,42 @@ import random
 import string
 
 from google.appengine.ext import db
+from models.user import User
+from models.post import Post
+from models.comment import Comment
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-SECRET = "Arma virumque cano"
+SECRET = "Arma virumque cano, Troiae qui primus ab oris Italiam, fato profugus"
 
-def render_str(template, **params):
-	""" renders HTML template into str """
-	t = jinja_env.get_template(template)
-	return t.render(params)
-
-def hash_str(s):
-	""" Hash a string """
-	return hmac.new(SECRET, s, hashlib.sha256).hexdigest()
-
-def make_secure_val(s):
-	""" turn string to secure value """
-	return "%s|%s" % (s, hash_str(s))
-
-def check_secure_val(h):
-	""" validate hash matches string """
-	val = h.split('|')[0]
-	if h == make_secure_val(val):
-		return val
-
-def make_salt(n=5):
-	""" returns generated salt """
-	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(n))
-
-def make_pw_hash(name, pw, salt=""):
-	""" Make a storable hash of the password """
-	# check if salt param set
-	if not salt:
-		salt = make_salt()
-
-	# convert key and msg from unicdoe str to byte str
-	key = bytearray(salt+SECRET, 'utf-8')
-	msg = bytearray(name+pw, 'utf-8')
-
-	# create hash
-	h = hmac.new(key, msg, hashlib.sha256).hexdigest()
-
-	#return hmac.new(salt+SECRET, name+pw, hashlib.sha256).hexdigest()+','+salt
-	return "%s,%s" % (h, salt)
-
-def valid_pw(name, pw, h):
-	""" validates user password """
-	salt = h.split(',')[1]
-	if h == make_pw_hash(name, pw, salt):
-		return True
+# datastore management
+def delete_all_comments():
+	""" clears Comment table"""
+	comments = Comment.all()
+	for c in comments:
+		c.delete()
 
 def delete_all_post():
+	""" Clears Post table """
  	posts = Post.all()
 	for p in posts:
 		p.delete()
 
+def delete_all_users():
+	""" Clears User table """
+	users = User.all()
+	for u in users:
+		u.delete()
 
-# Entity Classes
-class User(db.Model):
-	""" Entinty class for user """
-	username = db.StringProperty(required = True)
-	first_name = db.StringProperty(required = False)
-	last_name = db.StringProperty(required = False)
-	password = db.StringProperty(required = True)
-	email = db.StringProperty()
-	# implicit Property posts
-	# implicit property comments
-
-	@classmethod
-	def by_id(cls, uid):
-		return cls.get_by_id(uid)
-
-	@classmethod
-	def by_username(cls, username):
-		u = cls.all().filter('username =', username).get()
-		return u
-
-	@classmethod
-	def login(cls, username, pw):
-		u = cls.by_username(username)
-		if u and valid_pw(username, pw, u.password):
-			return u 
-
-
-class Post(db.Model):
-	""" Entity class for post """
-	author = db.ReferenceProperty(User, required = True, collection_name = 'posts')
-	subject = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
-	liked_by = db.ListProperty(str)
-	# implicit property comments
-
-	def render(self):
-		""" render text for more practical html """
-		self._render_text = self.content.replace('\n', '<br>')
-		return render_str("post.html", p = self)
-
-	def get_author_name(self):
-		if self.author:
-			return ('%s %s' % (self.author.first_name, self.author.last_name))
-
-	def get_likes(self):
-		if self.liked_by:
-			return len(self.liked_by)
-		else:
-			return 0
-
-class Comment(db.Model):
-	""" Entity class for Comment """
-	post = db.ReferenceProperty(Post, required = True, collection_name = 'comments')
-	author = db.ReferenceProperty(User, required = True, collection_name = 'comments')
-	comment = db.StringProperty(required = True)
+def clear_db():
+	""" Clears all tables """
+	delete_all_comments()
+	delete_all_post()
+	delete_all_users()
 
 # Handler Classes
 class Handler(webapp2.RequestHandler):
 	""" A class to add default handler functionality """
-
 	def write(self, *a, **kw):
 		""" Replaces response.out.write() for simplicity """
 		self.response.out.write(*a, **kw)
@@ -150,19 +68,33 @@ class Handler(webapp2.RequestHandler):
 		t = jinja_env.get_template(template)
 		return t.render(params)
 
+	def hash_str(self, s):
+		""" Hash a string """
+		return hmac.new(SECRET, s, hashlib.sha256).hexdigest()
+
+	def make_secure_val(self, s):
+		""" turn string to secure value """
+		return "%s|%s" % (s, self.hash_str(s))
+
+	def check_secure_val(self, h):
+		""" validate hash matches string """
+		val = h.split('|')[0]
+		if h == self.make_secure_val(val):
+			return val
+
 	def render(self, template, **kw):
 		""" Writes out template as a str """
 		self.write(self.render_str(template, **kw))
 
 	def set_secure_cookie(self, name, val):
 		""" sets a cookie with name and val """
-		cookie_val = make_secure_val(val)
+		cookie_val = self.make_secure_val(val)
 		self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
 
 	def read_secure_cookie(self, name):
 		""" reads a cookie with name """
 		cookie_val = self.request.cookies.get(name)
-		return cookie_val and check_secure_val(cookie_val)
+		return cookie_val and self.check_secure_val(cookie_val)
 
 	def login(self, user):
 		""" sets user_id cookie for login """
@@ -171,6 +103,13 @@ class Handler(webapp2.RequestHandler):
 	def logout(self):
 		""" removes user_id cookie """
 		self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+	def login_status(self):
+		if self.user:
+			return True
+	
+	def redirect_to_dashboard(self):
+		self.redirect("/blog/welcome")
 
 	def initialize(self, *a, **kw):
 		""" verify login status using cookie """
@@ -187,7 +126,7 @@ class Registration(Handler):
 
 	def get(self):
 		if self.user:
-				self.redirect("/blog/welcome")
+				self.redirect_to_dashboard()
 
 		self.render_signup()
 
@@ -197,7 +136,7 @@ class Registration(Handler):
 		last_name = self.request.get("last_name")
 		password = self.request.get("password")
 		verify = self.request.get("verify")
-		email = self.request.get("email")
+		email = self.request.get("email").lower()
 
 		if username and password and verify == password:
 			u = User.by_username(username)
@@ -206,7 +145,7 @@ class Registration(Handler):
 				self.render_signup(errorMessage=errorMessage)
 			else:
 				a = User(username = username, first_name = first_name,
-					last_name = last_name, password = make_pw_hash(username, password), 
+					last_name = last_name, password = User.make_pw_hash(username, password), 
 					email = email)
 				a.put()
 				self.login(a)
@@ -223,7 +162,7 @@ class Login(Handler):
 	""" Handler for login """
 	def get(self):
 		if self.user:
-			self.redirect("/blog/welcome")
+			self.redirect_to_dashboard()
 		
 		self.render('login-form.html')
 
@@ -245,13 +184,14 @@ class Logout(Handler):
 		self.logout();
 		self.redirect('/blog/signup')
 
-
+# Blog Handlers
 class BlogFront(Handler):
 	""" handler for front page of blog """
 	def get(self):
+
 		posts = Post.all().order('-created')
 		#posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC limit 10 ")
-		self.render('front.html', posts = posts)
+		self.render('front.html', posts = posts, logged_in = self.login_status())
 
 class Welcome(Handler):
 	""" Handler for user dashboard """
@@ -263,7 +203,7 @@ class Welcome(Handler):
 			for p in posts:
 				likes = likes + p.get_likes()
 
-			self.render("welcome.html", name = name, posts = posts, likes = likes)
+			self.render("welcome.html", logged_in = self.login_status(), name = name, posts = posts, likes = likes)
 		else:		
 			self.redirect("/blog/login")
 
@@ -277,13 +217,13 @@ class PostPage(Handler):
 			self.error(404)
 			return
 
-		self.render("permalink.html", post = post)
+		self.render("permalink.html", logged_in = self.login_status(), post = post)
 
 class NewPost(Handler):
 	""" Handler for inputing new post """
 	def render_new_post(self, subject = "", content = "", error = "", errorMessage = ""):
 		""" renders new post page from template """
-		self.render("newpost.html", subject = subject, content = content, error = error, errorMessage = errorMessage)
+		self.render("newpost.html",logged_in = self.login_status(), subject = subject, content = content, error = error, errorMessage = errorMessage)
 
 	def get(self):
 		if self.user:
@@ -352,7 +292,7 @@ class NewComment(Handler):
         
         post = Post.get_by_id(int(post_id))
         
-        self.render("newcomment.html", post = post)
+        self.render("newcomment.html", logged_in = self.login_status(), post = post)
 
     def post(self, post_id):
         post = Post.get_by_id(int(post_id))
@@ -372,7 +312,7 @@ class NewComment(Handler):
             c.put()
             self.redirect('/blog/%s' % str(post_id))
         else:
-            self.render("newcomment.html", post = post)
+            self.render("newcomment.html", logged_in = self.login_status(), post = post)
 
 class DeleteComment(Handler):
     def get(self, comment_id):
@@ -389,7 +329,7 @@ class DeleteComment(Handler):
 class MainPage(Handler):
 	""" Handler for main page of Blog """
 	def get(self):
-		delete_all_post()
+		#delete_all_post()
 
 		self.response.headers['Content-Type'] = 'text/plain'
 		visits = 0
